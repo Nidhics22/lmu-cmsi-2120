547bc090ead420404ee9bc0ae2a6c3e8f9a34f0e
Nidhi Prajapati <nidhiprajapati@Nidhis-Air.attlocal.net>
2022-10-06 00:14:01 -0700

    Updating ExpandableArrayStack & ExpandableArrayQueue classes

--- homework2/ExpandableArrayQueue.java
+++ homework2/ExpandableArrayQueue.java
@@ -1,4 +1,74 @@
 package homework2;
 
-public class ExpandableArrayQueue {
+import java.util.NoSuchElementException;
+
+public class ExpandableArrayQueue implements Queue{
+
+    private static final int initialCapacity = 16;
+    private int currentCapacity;
+    private Object[] q;
+    private int last;
+
+    public ExpandableArrayQueue() {
+        last = 0;
+        currentCapacity = initialCapacity;
+        q = new Object[initialCapacity];
+    }
+
+    private void increaseCapacity() {
+        currentCapacity = 2 * currentCapacity;
+        Object[] largeQ = new Object[currentCapacity];
+        System.arraycopy(q, 0, largeQ, 0, q.length);
+        q = largeQ;
+    }
+
+    private void decreaseCapacity() {
+        currentCapacity = currentCapacity/2;
+        Object[] smallQ = new Object[currentCapacity];
+        System.arraycopy(q, 0, smallQ, 0, smallQ.length);
+        q = smallQ;
+    }
+
+    @Override
+    public void enqueue(Object value) {
+        if(last == currentCapacity) {
+            increaseCapacity();
+        }
+        q[last] = value;
+        last++;
+    }
+
+    @Override
+    public Object dequeue() {
+        if(isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        Object dequeuedElement = q[0];
+        last--;
+        System.arraycopy(q, 1, q, 0, q.length-1);
+        q[last] = null;
+
+        if(currentCapacity > initialCapacity && last < currentCapacity/4) {
+            decreaseCapacity();
+        }
+        return dequeuedElement;
+    }
+
+    @Override
+    public Object peek() {
+        if (isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        return q[0];
+    }
+
+    @Override
+    public int size() {
+        return last;
+    }
+
+    public int capacity() {
+        return currentCapacity;
+    }
 }
+
--- homework2/ExpandableArrayStack.java
+++ homework2/ExpandableArrayStack.java
@@ -1,24 +1,71 @@
 package homework2;
 
-public class ExpandableArrayStack implements Stack{
-    @Override
-    public void push(Object item) {
+import java.util.NoSuchElementException;
 
+public class ExpandableArrayStack implements Stack {
+
+    private static final int initialCapacity = 16;
+    private int currentCapacity;
+    private Object[] stack;
+    private int last;
+
+    public ExpandableArrayStack() {
+        currentCapacity = 16;
+        last = 0;
+        stack = new Object[initialCapacity];
+    }
+
+    private void increaseCapacity() {
+        currentCapacity = currentCapacity * 2;
+        Object[] largeStack = new Object[currentCapacity];
+        System.arraycopy(stack, 0, largeStack, 0, stack.length);
+        stack = largeStack;
+    }
+
+    private void decreaseCapacity() {
+        currentCapacity = currentCapacity / 2;
+        Object[] smallStack = new Object[currentCapacity];
+        System.arraycopy(stack, 0, smallStack, 0, smallStack.length);
+        stack = smallStack;
+    }
+
+    @Override
+    public void push(Object value) {
+        if(last >= currentCapacity) {
+            increaseCapacity();
+        }
+        stack[last] = value;
+        last++;
     }
 
     @Override
     public Object pop() {
-        return null;
+        if(isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        last--;
+        Object poppedElement = stack[last];
+        stack[last] = null;
+        if(last > initialCapacity && last < currentCapacity/4) {
+            decreaseCapacity();
+        }
+        return poppedElement;
     }
 
     @Override
     public Object peek() {
-        return null;
+        if(isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        return stack[last-1];
     }
 
     @Override
     public int size() {
-        return 0;
+        return last;
     }
 
+    public int capacity() {
+        return currentCapacity;
+    }
 }
